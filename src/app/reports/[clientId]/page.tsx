1c23f7bc8797a0898df4f25a22b1317eaab2e97b
Victor Prisacariu <prisacariuvictor@gmail.com>
2024-02-21 22:33:15 +0200

    chore: navigation cleanup

--- src/app/reports/[clientId]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { AllClientsPromotionsReportsPage } from "@/Components/Reports/AllClientsPromotionsReportsPage";
-
-const DriversPage = ({ params }: { params: { clientId: string } }) => {
-  return <AllClientsPromotionsReportsPage clientId={params.clientId} />;
-};
-
-export default DriversPage;
