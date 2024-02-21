6932b842723b675a6dfdf9ed7252753d97028a43
Victor Prisacariu <prisacariuvictor@gmail.com>
2024-02-21 22:56:34 +0200

    fix: refactored promotions reports page

--- src/app/reports/clients/[clientId]/page.tsx
+++ src/app/reports/clients/[clientId]/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React from "react";
-import { AllClientsPromotionsReportsPage } from "@/Components/Reports/AllClientsPromotionsReportsPage";
+import { UniqueReportsPage } from "@/Components/Reports/AllClientsPromotionsReportsPage";
+import { usePathname } from "next/navigation";
 
 const DriversPage = ({ params }: { params: { clientId: string } }) => {
-  return <AllClientsPromotionsReportsPage clientId={params.clientId} />;
+  const pathName = usePathname();
+  const entity = pathName.split("/")[2];
+
+  return <UniqueReportsPage clientId={params.clientId} entity={entity} />;
 };
 
 export default DriversPage;
